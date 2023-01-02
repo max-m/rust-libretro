dd843f482827efd426970dfd5e986857ce4a59b0
Maximilian Mader <max@bastelstu.be>
2023-01-03 00:11:18 +0100

    Add proper attribution to kwohlfahrt/enum-tryfrom in rust_libretro_sys_proc::TryFromPrimitive

--- rust-libretro-sys/rust-libretro-sys-proc/src/lib.rs
+++ rust-libretro-sys/rust-libretro-sys-proc/src/lib.rs
@@ -29,6 +29,30 @@ fn filter_primitive_type_attr(attr: &syn::Attribute) -> Option<(String, Span)> {
     None
 }
 
+/// This macro is based on [enum-tryfrom](https://github.com/kwohlfahrt/enum-tryfrom) (MIT).
+///
+/// Original license:
+/// > MIT License
+/// >
+/// > Copyright (c) 2017 Kai Wohlfahrt
+/// >
+/// > Permission is hereby granted, free of charge, to any person obtaining a copy
+/// > of this software and associated documentation files (the "Software"), to deal
+/// > in the Software without restriction, including without limitation the rights
+/// > to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// > copies of the Software, and to permit persons to whom the Software is
+/// > furnished to do so, subject to the following conditions:
+/// >
+/// > The above copyright notice and this permission notice shall be included in all
+/// > copies or substantial portions of the Software.
+/// >
+/// > THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// > IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// > FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// > AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// > LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// > OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// > SOFTWARE.
 #[proc_macro_derive(TryFromPrimitive)]
 pub fn from_primitive(input: TokenStream) -> TokenStream {
     let input: DeriveInput = syn::parse(input).unwrap();
